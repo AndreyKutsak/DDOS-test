863f44406ffc92e3710a6cb4d114db5888fd4bb9
AndreyKutsak <andreykutsak.aak@gmail.com>
2022-03-05 00:48:55 +0200

    add api  referer headers for fetch query

--- hosts.php
+++ hosts.php
@@ -1,4 +1,12 @@
-<?php 
- $hosts = json_encode(file_get_contents(''), true);//список переліком сайтів для атаки
-echo $hosts;
+<?php
+    $siteList=Array();
+    $hosts = json_decode(file_get_contents('https://gitlab.com/cto.endel/atack_hosts/-/raw/master/hosts.json'), true);//список переліком сайтів для атаки     
+    for ($i=0; $i <count($hosts) ; $i++) {        
+       $data = @file_get_contents($hosts[array_rand($hosts)]);
+        if($data){
+            $sites=json_decode($data,true);  
+            array_push($siteList,$sites['site']['url']);
+        }     
+    }
+    echo json_encode($siteList);
 ?>
