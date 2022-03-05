9f4f0e88bfb095f6554da0c12452c638a9fdde8c
AndreyKutsak <andreykutsak.aak@gmail.com>
2022-03-05 11:26:52 +0200

    add siteList

--- hosts.php
+++ hosts.php
@@ -1,12 +1,10 @@
 <?php
     $siteList=Array();
-    $hosts = json_decode(file_get_contents('https://gitlab.com/cto.endel/atack_hosts/-/raw/master/hosts.json'), true);//список переліком сайтів для атаки     
-    for ($i=0; $i <count($hosts) ; $i++) {        
-       $data = @file_get_contents($hosts[array_rand($hosts)]);
-        if($data){
-            $sites=json_decode($data,true);  
-            array_push($siteList,$sites['site']['url']);
-        }     
-    }
-    echo json_encode($siteList);
+    $hosts = json_decode(file_get_contents('https://gitlab.com/cto.endel/atack_api/-/raw/master/sites.json'), true);//список переліком сайтів для атаки     
+          foreach($hosts as $link){              
+              if($link['atack']==1){                
+                    array_push($siteList,$link['page']);             
+              }
+          }         
+    print_r(json_encode($siteList));   
 ?>
